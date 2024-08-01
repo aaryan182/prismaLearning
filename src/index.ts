463215911326d2bc870f4f06158aba3f1f144ed7
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-08-01 10:55:19 +0530

    update operation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+
+// async function insertUser(username: string , password: string , firstName: string , lastName: string ){
+//     const res = await prisma.user.create({
+//         data: {
+//             email: username,
+//             password,
+//             firstName,
+//             lastName
+//         },
+//         select: {
+//             id: true,
+//             password: true
+//         }
+//     })
+//     console.log(res);
+// }
+
+// insertUser("test1", "test", "test", "test")
+
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-async function insertUser(username: string , password: string , firstName: string , lastName: string ){
-    const res = await prisma.user.create({
-        data: {
-            email: username,
-            password,
-            firstName,
-            lastName
-        },
-        select: {
-            id: true,
-            password: true
-        }
-    })
-    console.log(res);
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
 }
 
-insertUser("test1", "test", "test", "test")
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  prisma.user.update({
+    where: { username: username },
+    data: {
+      firstName,
+      lastName
+    }
+  })
+}
